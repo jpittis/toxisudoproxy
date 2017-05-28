5330a811594c61ca72e18007bb67c4ee25786917
Jake Pittis <jakepittis@gmail.com>
2017-05-28 12:09:38 -0400

    add latency toxic

--- main.rb
+++ main.rb
@@ -60,40 +60,91 @@ class DropOutputToxic < Toxic
   end
 
   def enable!
-    `sudo iptables -I OUTPUT -p tcp -o lo --dport #{port} -j DROP`
+    `iptables -I OUTPUT -p tcp -o lo --dport #{port} -j DROP`
   end
 
   def disable!
-    `sudo iptables -D OUTPUT -p tcp -o lo --dport #{port} -j DROP`
+    `iptables -D OUTPUT -p tcp -o lo --dport #{port} -j DROP`
+  end
+end
+
+class LatencyToxic < Toxic
+  def initialze(delay)
+    @delay = delay
+  end
+
+  def enable!
+    `tc qdisc add latency lo root netem delay #{delay}ms`
+  end
+
+  def disable!
+    `tc qdisc del latency lo root netem`
+  end
+end
+
+class ToxicTest < Test::Unit::TestCase
+  def setup
+    @host = '127.0.0.1'
+    @port = '1234'
+    @server = Server.new(@port)
+  end
+
+  protected
+
+  def can_connect_to_host?(timeout = 0.1)
+    start = Time.now
+    http = Net::HTTP.new(@host, @port)
+    http.open_timeout = timeout
+    http.get('/')
+    (Time.now - start)
+  rescue Net::OpenTimeout
+    nil
+  end
+
+  def with_server
+    @server.with_server do
+      yield
+    end
+  end
+
+  def with_toxic
+    @toxic.with_enabled do
+      yield
+    end
   end
 end
 
 class DropOutputToxicTest < Test::Unit::TestCase
   def setup
-    @host = '127.0.0.1'
-    @port = '1234'
+    super
     @toxic = DropOutputToxic.new(@host, @port)
-    @server = Server.new(@port)
   end
 
   def test_toxic_drops_output_and_cleans_up
-    @server.with_server do
+    with_server do
       assert can_connect_to_host?
-      @toxic.with_enabled do
+      with_toxic do
         refute can_connect_to_host?
       end
       assert can_connect_to_host?
     end
   end
+end
 
-  private
+class LatencyToxicTest < ToxicTest
+  def setup
+    super
+    @delay = 50
+    @toxic = LatencyToxic.new(@delay)
+  end
 
-  def can_connect_to_host?
-    http = Net::HTTP.new(@host, @port)
-    http.open_timeout = 0.1
-    http.get('/')
-    true
-  rescue Net::OpenTimeout
-    false
+  def test_toxic_adds_latency
+    with_server do
+      p can_connect_to_host?
+      with_toxic do
+        p can_connect_to_host?
+      end
+      p can_connect_to_host?
+    end
   end
 end
