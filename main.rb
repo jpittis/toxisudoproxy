baf9dc7853ec0e0e801eff9a8cf9c70e16e5873e
Jake Pittis <jakepittis@gmail.com>
2017-05-28 11:58:23 -0400

    reformat

--- main.rb
+++ main.rb
@@ -1,21 +1,22 @@
 require 'rack'
 require 'net/http'
+require 'test/unit'
 
 class Toxic
-	def with_enabled
-		enable!
-		yield
-	ensure
-		disable!
-	end
+  def with_enabled
+    enable!
+    yield
+  ensure
+    disable!
+  end
 
-	def enable!
-		raise NotImplementedError
-	end
+  def enable!
+    raise NotImplementedError
+  end
 
-	def disable!
-		raise NotImplementedError
-	end
+  def disable!
+    raise NotImplementedError
+  end
 end
 
 class Server
@@ -25,7 +26,7 @@ class Server
     @port = port
   end
 
-  def call(env)
+  def call(_env)
     ['200', { 'Content-Type' => 'text/html' }, ['success']]
   end
 
@@ -44,57 +45,55 @@ class Server
 
   def run
     Rack::Handler::WEBrick.run(self,
-			       Port: port,
+                               Port: port,
                                AccessLog: [],
                                Logger: WEBrick::Log.new('/dev/null'))
   end
 end
 
 class DropOutputToxic < Toxic
-	attr_reader :host, :port
+  attr_reader :host, :port
 
-	def initialize(host, port)
-		@host = host
-		@port = port
-	end
+  def initialize(host, port)
+    @host = host
+    @port = port
+  end
 
-	def enable!
-		`sudo iptables -I OUTPUT -p tcp -o lo --dport #{port} -j DROP`
-	end
+  def enable!
+    `sudo iptables -I OUTPUT -p tcp -o lo --dport #{port} -j DROP`
+  end
 
-	def disable!
-		`sudo iptables -D OUTPUT -p tcp -o lo --dport #{port} -j DROP`
-	end
+  def disable!
+    `sudo iptables -D OUTPUT -p tcp -o lo --dport #{port} -j DROP`
+  end
 end
 
-require "test/unit"
-
 class DropOutputToxicTest < Test::Unit::TestCase
-	def setup
-		@host = '127.0.0.1'
-		@port = '1234'
-		@toxic = DropOutputToxic.new(@host, @port)
-                @server = Server.new(@port)
-	end
+  def setup
+    @host = '127.0.0.1'
+    @port = '1234'
+    @toxic = DropOutputToxic.new(@host, @port)
+    @server = Server.new(@port)
+  end
 
-	def test_toxic_drops_output_and_cleans_up
-		@server.with_server do
-			assert can_connect_to_host?
-			@toxic.with_enabled do
-				refute can_connect_to_host?
-			end
-			assert can_connect_to_host?
-		end
-	end
+  def test_toxic_drops_output_and_cleans_up
+    @server.with_server do
+      assert can_connect_to_host?
+      @toxic.with_enabled do
+        refute can_connect_to_host?
+      end
+      assert can_connect_to_host?
+    end
+  end
 
-	private
+  private
 
-	def can_connect_to_host?
-		http = Net::HTTP.new(@host, @port)
-		http.open_timeout = 0.1
-		http.get('/')
-		true
-	rescue Net::OpenTimeout
-		false
-	end
+  def can_connect_to_host?
+    http = Net::HTTP.new(@host, @port)
+    http.open_timeout = 0.1
+    http.get('/')
+    true
+  rescue Net::OpenTimeout
+    false
+  end
 end
